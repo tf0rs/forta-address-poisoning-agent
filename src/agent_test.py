76c305ee0861049c3cc7e45f4a140241af03e76f
Trevor Forsyth <trevorforsyth@Trevors-MBP.fritz.box>
2023-05-16 08:24:38 +0100

    Updated sharding with testing

--- src/agent_test.py
+++ src/agent_test.py
@@ -1,21 +1,72 @@
 from unittest.mock import MagicMock
 import agent
-from forta_agent import create_transaction_event, TransactionEvent
+from forta_agent import create_transaction_event, TransactionEvent, Web3, get_json_rpc_url
 from web3_mock import *
 from rules import AddressPoisoningRules
 from blockexplorer_mock import BlockExplorerMock
+from blockexplorer import BlockExplorer
+import timeit
 
 
 w3 = Web3Mock()
 blockexplorer = BlockExplorerMock(w3.eth.chain_id)
 heuristic = AddressPoisoningRules()
 
+real_w3 = Web3(Web3.HTTPProvider(get_json_rpc_url()))
+real_blockexplorer = BlockExplorer(real_w3.eth.chain_id)
+
 class TestAddressPoisoningAgent:
 
-    # Not sure if this is needed...
-    def test_parse_logs_for_transfer_and_approval_info(self):
-        agent.initialize()
-        pass
+    def test_transaction_processing_speed(self):
+
+        global transaction_to_eoa
+        transaction_to_eoa = create_transaction_event({
+            'transaction': {
+                'hash': "0x4a419b16152cc6513db84bdfb94818827ef8e64c2bc52f9a95f960c47ef02817",
+                'from': "0x5b7d833a4aa182bddd754db797d43d3c29c171a0",
+                'value': 100,
+                'to': "0xb2880739e3bd3f535d38760cd8f2ee058737341b",
+            }
+        })
+
+        global transaction_to_contract
+        transaction_to_contract = create_transaction_event({
+            'transaction': {
+                'hash': "0x00159936a7412553e0736c6f4d6cf91028ddd2defb8a3427318f7599b941f42d",
+                'from': "0xb2880739e3bd3f535d38760cd8f2ee058737341b",
+                'value': 0,
+                'to': "0xa9d7c7466b9d0a76397d5a226da0024fdcff0ee1",
+            }
+        })
+
+        global transaction_to_verified_contract
+        transaction_to_verified_contract = create_transaction_event({
+            'transaction': {
+                'hash': "0x0af2f2d106bff1950805d610e927344a2477b8be138ab4d4269e72b9aab5ddec",
+                'from': "0x2a038e100f8b85df21e4d44121bdbfe0c288a869",
+                'value': 0,
+                'to': "0xba8da9dcf11b50b03fd5284f164ef5cdef910705",
+            }
+        })
+
+        global fake_token_phishing_tx
+        fake_token_phishing_tx = create_transaction_event({
+            'transaction': {
+                'hash': "0xae818f01e8e911da67d87a577ecfc04443cad96ac3e099b2dac44633594d7311",
+                'from': "0xb2880739e3bd3f535d38760cd8f2ee058737341b",
+                'value': 0,
+                'to': "0xa9d7c7466b9d0a76397d5a226da0024fdcff0ee1",
+            }
+        })
+
+        processing_runs = 10
+
+        processing_time_to_eoa = timeit.timeit('agent.detect_address_poisoning(real_w3, real_blockexplorer, heuristic, transaction_to_eoa)', number=processing_runs, globals=globals()) * 1000 / processing_runs
+        processing_time_to_contract = timeit.timeit('agent.detect_address_poisoning(real_w3, real_blockexplorer, heuristic, transaction_to_contract)', number=processing_runs, globals=globals()) * 1000 / processing_runs
+        processing_time_to_verified_contract = timeit.timeit('agent.detect_address_poisoning(real_w3, real_blockexplorer, heuristic, transaction_to_verified_contract)', number=processing_runs, globals=globals()) * 1000 / processing_runs
+        processing_time_fake_token = timeit.timeit('agent.detect_address_poisoning(real_w3, real_blockexplorer, heuristic, fake_token_phishing_tx)', number=processing_runs, globals=globals()) * 1000 / processing_runs
+
+        assert (processing_time_to_eoa * 0.33 + processing_time_to_contract * 0.33 + processing_time_to_verified_contract * 0.33 + processing_time_fake_token * 0.01)/8 < 125, f"Time is {(processing_time_to_eoa * 0.33 + processing_time_to_contract * 0.33 + processing_time_to_verified_contract * 0.33 + processing_time_fake_token * 0.01)/8}, normal: {processing_time_to_eoa} - {processing_time_to_contract} - {processing_time_to_verified_contract} - {processing_time_fake_token}"
 
 
     def test_transfer_to_eoa(self):
